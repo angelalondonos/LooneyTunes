b0f1ac09b2955f4c37d114767124baab0bb0c2a4
ANGELA MARIA LONDONO <30359708+angelm05@users.noreply.github.com>
2017-10-20 23:02:47 +0000

    Encabezado

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/looneytunes/LooneyTunesActivity.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.android.electiva.looneytunes;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Angela Londoño
+ * @author Rodrigo Ramirez
+ * @author Cristian Agudelo
+ *
+ */
+
+public class LooneyTunesActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public NavigationView navView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looney_tunes);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navView = (NavigationView)findViewById(R.id.navview);
+        navView.setItemIconTintList(null);
+
+    }
+}
