b2e5972207f89e5a191f0c74189a25a7585e0962
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2017-11-06 20:46:01 -0500

    Add files for string in english and spanish internationalitation

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/looneytunes/util/Utilidades.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.android.electiva.looneytunes.util;
+
+/**
+ * Created by andres on 6/11/17.
+ */
+
+public class Utilidades {
+
+    public final static String MIS_PREFERENCIAS = "MisPreferencias";
+    public final static String LENGUAJE_DE_PREFERENCIA = "languaje_preferences";
+    public final static String LENGUAJE_ES = "es";
+    public final static String LENGUAJE_EN = "en";
+}
